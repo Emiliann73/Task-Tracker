e94f94491fff18d9fa7a82d6bd5129b86d8d5d4f
Emiliann73 <84806250+Emiliann73@users.noreply.github.com>
2025-08-27 15:49:16 -0600

    Agregue la funcion de agregar nuevas tareas

--- Task-Tracker/task_cli_project.py
+++ Task-Tracker/task_cli_project.py
@@ -1,1 +1,19 @@
+#add a task
+#remove a task
+#edit a task
+#add a description
+#edit a description
 
+#inicial task list
+tasks = []
+
+#allow the user to add a new task
+def addTask(all_tasks):
+    new_task = input("Add a task: ")#ask the user for input and its going to store that in new_task variable
+    all_tasks.append(new_task)
+    
+    for task in all_tasks:
+        print(task)
+
+#start application
+addTask(tasks)
