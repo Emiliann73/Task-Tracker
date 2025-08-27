f16f4aff425b9bf53441cb618aa260b97ef407e0
Emiliann73 <84806250+Emiliann73@users.noreply.github.com>
2025-08-27 16:05:13 -0600

    Se agrego la funcion para hacer otra operacion y la funcion de mostrar la lista de tareas junto con las tareas ya puestas

--- Task-Tracker/task_cli_project.py
+++ Task-Tracker/task_cli_project.py
@@ -7,13 +7,39 @@
 #inicial task list
 tasks = []
 
+#Auxiliary function
+def displayTasks(all_tasks):
+    print("\nYour tasks: ")
+    for index, task in enumerate(all_tasks):
+        print(f"{index+1}: {task}")
+
+#allows the user choose  a new operation
+def new_operation(all_tasks):
+    operation = input("Press 'A' to add new task, 'E' to edit a task, 'R' to remove a task, 'F' to quir the application:  ")
+
+    if operation == 'A':
+        addTask(all_tasks)
+    elif operation == 'E':
+        pass
+    elif operation == 'R':
+        pass
+    elif operation == 'F':
+        pass
+    else:
+        new_operation(all_tasks)
+        
+    
 #allow the user to add a new task
 def addTask(all_tasks):
     new_task = input("Add a task: ")#ask the user for input and its going to store that in new_task variable
     all_tasks.append(new_task)
     
-    for task in all_tasks:
-        print(task)
+    displayTasks(all_tasks)
+        
+    new_operation(all_tasks)
+
+
 
 #start application
 addTask(tasks)
+
